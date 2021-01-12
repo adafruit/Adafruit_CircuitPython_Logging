a08cb7b0c279e95ad0da98e35f45a0cb47dde885
dherrada <dylan.herrada@adafruit.com>
2021-01-12 12:57:13 -0500

    Ran pre-commit, added licenses

--- examples/logging_simpletest.py
+++ examples/logging_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 # pylint:disable=undefined-variable,wildcard-import,no-name-in-module
 # pylint:disable=no-member
 
