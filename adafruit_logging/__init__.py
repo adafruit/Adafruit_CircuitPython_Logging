ecc450dbb0bc7c38e0790a8e2af8800e3a20cdee
Alec Delaney <tekktrik@gmail.com>
2021-10-28 11:52:16 -0400

    Move FileHandler to extensions.py

--- adafruit_logging/__init__.py
+++ adafruit_logging/__init__.py
@@ -91,38 +91,6 @@ class PrintHandler(LoggingHandler):
         print(self.format(level, msg))
 
 
-class FileHandler(LoggingHandler):
-    """File handler for working with log files off of the microcontroller (like
-    an SD card)
-
-    :param filepath: The filepath to the log file
-    :param mode: Whether to write ('w') or append ('a'); default is to append
-    """
-
-    def __init__(self, filepath: str, mode: str = "a"):
-        self.logfile = open(filepath, mode, encoding="utf-8")
-
-    def close(self):
-        """Closes the file"""
-        self.logfile.close()
-
-    def format(self, level: int, msg: str):
-        """Generate a string to log
-
-        :param level: The level of the message
-        :param msg: The message to format
-        """
-        return super().format(level, msg) + "\r\n"
-
-    def emit(self, level: int, msg: str):
-        """Generate the message and write it to the UART.
-
-        :param level: The level of the message
-        :param msg: The message to log
-        """
-        self.logfile.write(self.format(level, msg))
-
-
 # The level module-global variables get created when loaded
 # pylint:disable=undefined-variable
 
