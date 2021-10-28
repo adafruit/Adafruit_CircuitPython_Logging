0c8a6a3a690903fd3238ecbcc425522902f740ae
Alec Delaney <tekktrik@gmail.com>
2021-10-28 10:33:53 -0400

    Updated example to initialize SD card

--- examples/logging_filehandler.py
+++ examples/logging_filehandler.py
@@ -1,9 +1,24 @@
 # SPDX-FileCopyrightText: 2021 Alec Delaney
 # SPDX-License-Identifier: MIT
 
+import board
+import busio
+from digitalio import DigitalInOut
+import storage
+import adafruit_sdcard
 import adafruit_logging as logging
 
-# Initialize log functionality on a writable medium, like an SD card
+# Get chip select pin depending on the board, this one is for the Feather M4 Express
+sd_cs = board.D10
+
+# Set up an SD card to write to
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+cs = DigitalInOut(sd_cs)
+sdcard = adafruit_sdcard.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+
+# Initialize log functionality
 log_filepath = "/sd/testlog.log"
 logger = logging.getLogger("testlog")
 file_handler = logging.FileHandler(log_filepath)
